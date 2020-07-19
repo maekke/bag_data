0b0a8ab7f04f99bcd95b8b4138ae20427177a2b4
Markus Meier <maekke@gentoo.org>
2020-07-19 21:43:51 +0200

    add parser for weekly per canton reports
    - for now this supports the number of performed PCR tests

---
+++ parse-canton-data.py
@@ -0,0 +1,46 @@
+#!/bin/env python
+
+import sys
+import re
+
+from common import *
+
+if len(sys.argv) != 3:
+    raise ValueError('please provide a canton and file name as input!')
+
+canton = sys.argv[1]
+filename = sys.argv[2]
+
+txt = pdf_to_text(filename)
+
+"""
+Coronavirus-Krankheit-2019 (COVID-19)
+Eidgen<C3><B6>ssisches Departement des Innern EDI
+Bundesamt f<C3><BC>r Gesundheit BAG
+Direktionsbereich <C3><96>ffentliche Gesundheit
+Situationsbericht zur epidemiologischen Lage in der Schweiz
+und im F<C3><BC>rstentum Liechtenstein - Woche 28 (06.-12.07.2020)
+"""
+
+week = search(r'Liechtenstein - Woche (\d+)', txt)
+
+"""
+Canton, tests of previous-week then current-week
+
+AG 5478 3588 808 529 1.3 1.8
+AI 96 55 595 341 0.0 0.0
+AR 391 249 708 451 0.5 1.2
+BE 6924 4652 669 449 0.4 0.9
+...
+"""
+start = txt.find('Anzahl PCR-Tests in der Schweiz')
+if start > 0:
+    start = txt.find(r' AG ', start)
+else:
+    start = 0
+end = txt.find('Tabelle 4. Durchgeführte Tests nach Kalenderwoche', start)
+if start > 0 and end > start:
+    tests_table = txt[start:end]
+    number_of_tests = txt_to_int(search(r'(\n\s+)?{}\s+\d+\s+(\d+)'.format(canton), tests_table, index=2))
+
+print('{},{},{}'.format(week, number_of_tests or '', filename))
---
+++ run-canton.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+if [[ -d tmp ]] ; then
+	rm -r tmp
+fi
+
+cantons=(
+	"AG"
+	"AI"
+	"AR"
+	"BE"
+	"BL"
+	"BS"
+	"FR"
+	"GE"
+	"GL"
+	"GR"
+	"JU"
+	"LU"
+	"NE"
+	"NW"
+	"OW"
+	"SG"
+	"SH"
+	"SO"
+	"SZ"
+	"TG"
+	"TI"
+	"UR"
+	"VD"
+	"VS"
+	"ZG"
+	"ZH"
+	"FL"
+)
+
+out_dir="daten_pro_kanton"
+if [[ ${1} == "--all" ]] ; then
+	for canton in ${cantons[*]} ; do
+		echo "week,total_number_of_tests,source_file" > ${out_dir}/bag_data_${canton}.csv
+	done
+fi
+
+mkdir -p tmp
+mkdir -p ${out_dir}
+
+pushd tmp > /dev/null
+
+if [[ ${1} == "--all" ]] ; then
+	wget https://www.bag.admin.ch/dam/bag/de/dokumente/mt/k-und-i/aktuelle-ausbrueche-pandemien/2019-nCoV/bisherige-lageberichte-zip.zip.download.zip/Epidemiologische_Lage_Schweiz.zip
+	unzip Epidemiologische_Lage_Schweiz.zip
+	pushd Deutsch > /dev/null
+
+	for i in *KW*.pdf ; do
+		for canton in ${cantons[*]} ; do
+			python ../../parse-canton-data.py ${canton} "${i}" >> ../../${out_dir}/bag_data_${canton}.csv
+		done
+	done
+
+	popd > /dev/null
+else
+	wget https://www.bag.admin.ch/dam/bag/de/dokumente/mt/k-und-i/aktuelle-ausbrueche-pandemien/2019-nCoV/covid-19-woechentlicher-lagebericht.pdf.download.pdf/BAG_COVID-19_Woechentliche_Lage.pdf
+	for canton in ${cantons[*]} ; do
+		python ../parse-canton-data.py ${canton} BAG_COVID-19_Woechentliche_Lage.pdf >> ../${out_dir}/bag_data_${canton}.csv
+	done
+fi
+
+popd > /dev/null
+rm -r tmp
