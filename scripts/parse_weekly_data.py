951b2b591e1554fdb74858acfda2f8eb4e9060a6
Markus Meier <maekke@gentoo.org>
2020-09-25 17:59:20 +0200

    adapt positivity rate detection for W38

--- scripts/parse_weekly_data.py
+++ scripts/parse_weekly_data.py
@@ -25,13 +25,15 @@ def parse_weekly_data(filename):
 
     positivity_rate = None
     positivity_pos = txt.find('\nPositivit')
+    if positivity_pos == -1:
+        positivity_pos = txt.find('\nAnteil positive Tests')
     if positivity_pos > 0:
         positivity_pos += 1
         positivity_end_pos = txt.find('\n', positivity_pos)
         assert positivity_end_pos > positivity_pos
         line = txt[positivity_pos:positivity_end_pos]
         line = re.sub(r'\s+', r' ', line)
-        positivity_rate = line.split(' ')[-2]
+        positivity_rate = line.split(' ')[-1]
         positivity_rate = c.txt_to_float(positivity_rate.replace('%', ''))
 
     print('{},{},{},{}'.format(week, tot_tests or '', positivity_rate or '',
