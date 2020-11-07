d482c0c5cd49851f67f0b7b4c6a3e0be63cd11f0
Markus Meier <maekke@gentoo.org>
2020-11-07 14:38:26 +0100

    wire up scrape_foph_page.py in run-latest.sh

--- scripts/run-latest.sh
+++ scripts/run-latest.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 
-if [[ -d tmp ]] ; then
-	rm -r tmp
-fi
-
 out_file="bag_data.csv"
-
-mkdir -p tmp
-pushd tmp > /dev/null
-
-wget https://www.bag.admin.ch/dam/bag/de/dokumente/mt/k-und-i/aktuelle-ausbrueche-pandemien/2019-nCoV/covid-19-lagebericht.pdf.download.pdf/COVID-19_Epidemiologische_Lage_Schweiz.pdf
-
-for i in *.pdf ; do
-	python ../scripts/parse_data.py "${i}" >> ../${out_file}
-done
-
-popd > /dev/null
-rm -r tmp
+python ./scripts/scrape_foph_page.py >> ./${out_file}
